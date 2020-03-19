a8159b7952414c4d652b4af14664cfaed56fb71e
tadashi-aikawa <syou.maman@gmail.com>
2020-03-19 13:10:51 +0900

    :skull: Fix not working

--- contents.js
+++ contents.js
@@ -2,39 +2,45 @@ function inSelectorAny(task, selectors) {
   return selectors.some(s => !!task.querySelector(s));
 }
 
-function styleTimeDivider(mutationRecords) {
+function styleTimeDivider() {
   console.debug("todoist-owl-style: fire");
   const tasks = document.querySelectorAll(".task_item_details");
   tasks.forEach(task => {
-    if (inSelectorAny(task, [
-      '* img[alt="⏲"]',
-      '* img[alt="🌅"]',
-      '* img[alt="🏢"]',
-      '* img[alt="🍙"]',
-      '* img[alt="🏠"]'
-    ])) {
+    if (
+      inSelectorAny(task, [
+        '* img[alt="⏲"]',
+        '* img[alt="🌅"]',
+        '* img[alt="🏢"]',
+        '* img[alt="🍙"]',
+        '* img[alt="🏠"]'
+      ])
+    ) {
       task.classList.add("time-divider");
     }
-    if (inSelectorAny(task, [
-      '* img[alt="📆"]'
-    ])) {
+    if (inSelectorAny(task, ['* img[alt="📆"]'])) {
       task.classList.add("calendar-task");
     }
   });
 }
 
-// クエリだけ変わったとき用
-(new MutationObserver(styleTimeDivider)).observe(
-  document.querySelector("#content"),
-  { childList: true }
-);
-
-// その他の変更検知
-function setObserver() {
-  (new MutationObserver(styleTimeDivider)).observe(
-    document.querySelector("#editor"),
+function main() {
+  // クエリだけ変わったとき用
+  new MutationObserver(styleTimeDivider).observe(
+    document.querySelector("#content"),
     { childList: true }
   );
+
+  // その他の変更検知
+  function setObserver() {
+    new MutationObserver(styleTimeDivider).observe(
+      document.querySelector("#editor"),
+      { childList: true }
+    );
+  }
+  window.onhashchange = setObserver;
+  setObserver();
+
+  styleTimeDivider();
 }
-window.onhashchange = setObserver;
-setObserver();
+
+setTimeout(main, 2000);
